ba7caeac9e946e2f070720d0ff05a87b307d96ba
plower222 <x-plower@yandex.ru>
2022-05-29 17:32:26 +0400

    реализован калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  number1: 0,
+  number2: 0,
+  read(a,b) {
+    this.number1 = a;
+    this.number2 = b;
+  },
+  sum() {
+    return this.number1 + this.number2;
+  },
+  mul() {
+    return this.number1 * this.number2;
+  }
+
 };
+
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
+
