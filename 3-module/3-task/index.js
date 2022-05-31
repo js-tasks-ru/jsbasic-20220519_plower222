2236ed407df153d1f858e7df806f7b97510e542e
plower222 <x-plower@yandex.ru>
2022-05-31 20:22:36 +0400

    реализована функция camelize(str) по преобразованию строки в вид CamelCase

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,22 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split("-");
+  let arr = upperCaseFirstElement(str);
+  return arr.join('');
 }
+
+function upperCaseFirstElement(arr) {
+  let tempArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      tempArr.push(arr[0]);
+    }
+    else {
+      let tempValue = [...arr[i]];
+      tempValue[0] = tempValue[0].toUpperCase();
+      tempArr.push(tempValue.join(""));
+    }
+  }
+  return tempArr;
+}
+
+// console.log(camelize('my-short-string'));
