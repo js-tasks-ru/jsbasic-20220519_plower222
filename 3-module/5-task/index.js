a3ed79b4fcb1d2a9c79c1976c51c210d102321fa
plower222 <x-plower@yandex.ru>
2022-06-01 21:05:05 +0400

    реализована функция нахождения min/max в строке

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = [];
+  let arrTemp = str.split(" ");
+  for (let value of arrTemp) {
+    if (typeof +value === 'number' && isFinite(+value)) {
+      arr.push(+value);
+    }
+  }
+  return  {min: Math.min(...arr), max: Math.max(...arr)};
 }
+
+let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+
+console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
